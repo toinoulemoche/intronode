1543f42de97a8c22730846136cf7668164e9ca2d
toinoulemoche <avaurs28@gmail.com>
2022-04-28 09:49:11 +0200

    Récupère la variable d'environnement MAVARIABLE et l'affiche

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("HELLO WORLD");
+
+const mavariable = process.env["MAVARIABLE"];
+console.log(mavariable);
+
+const variablepasdef = process.env["VARIABLEPASDEF"];
+console.log(variablepasdef);
