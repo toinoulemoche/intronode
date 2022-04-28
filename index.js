06fa4ab7d0faf9fbb27c33ce4e82be1f171b182e
toinoulemoche <avaurs28@gmail.com>
2022-04-28 10:25:45 +0200

    detection de l'environnement

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//Le fichier .env permet d'instancier des variables d'environnements du
+//projet, pour définir par ex la connexion à la BDD ou l'environnement local de travail
+
 console.log("HELLO WORLD");
 
 const mavariable = process.env["MAVARIABLE"];
@@ -5,3 +8,12 @@ console.log(mavariable);
 
 const variablepasdef = process.env["VARIABLEPASDEF"];
 console.log(variablepasdef);
+
+console.log(process.env.NODE_ENV);
+
+if(process.env.NODE_ENV == 'development'){
+	console.log("JE SUIS EN DEVELOPEMENT");
+}
+else{
+	console.log("JE SUIS EN PRODUCTION");
+}
