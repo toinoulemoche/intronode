4950cad19e44b73d541432dd53df26b8313ff5ad
toinoulemoche <avaurs28@gmail.com>
2022-04-29 16:33:45 +0200

    exo7 ajout du chrono

---
+++ exo7_middleware/server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  try {
+    // ICI MIDDLEWARE A EXECUTER AVANT DE TRAITER LA REQUETE.
+    console.log(req.httpVersion, req.url, req.method);
+    // FIN MIDDLEWARE
+    if (req.url === "/") {
+      console.timer("chrono");
+      res.writeHead(200, {'content-type':'text/html'});
+      res.write('<h1>HOMEPAGE</h1>');
+      res.end();
+    } else {
+      res.writeHead(404, {'content-type':'text/html'});
+      res.write('<h1>404 Not Found</h1>');
+      res.end()
+    }
+  } catch (err) {
+    res.writeHead(500, {'content-type':'text/html'});
+    res.write('<h1>500 Internal Server Error</h1>');
+    res.end()
+  }
+} );
+
+server.listen(3000);
+console.log('Server écoute sur le port 3000');
