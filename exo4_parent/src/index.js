c550395a10abbb122fbfa7102e1d7fe1a2c6557b
toinoulemoche <avaurs28@gmail.com>
2022-04-29 10:15:39 +0200

    completion de l'exercice 4 et de l'exercice 3 (problème avec Buffer)

--- exo4_parent/src/index.js
+++ exo4_parent/src/index.js
@@ -1,3 +1,8 @@
 const path = require('path');
+const fs = require('fs');
 
-path.join('/foo', 'bar', 'baz/asdf', 'quux', '..');
+const contenuIndexHtml = fs.readFileSync(path.join(__dirname, '../assets/index.html'), 'utf-8');
+const contenuLogTxt = fs.readFileSync(path.join(__dirname, 'log.txt'), 'utf-8');
+
+console.log(contenuIndexHtml);
+console.log(contenuLogTxt);
