ca8667e2ae410cff641f50f2269c743db6c5523e
toinoulemoche <avaurs28@gmail.com>
2022-05-02 10:37:53 +0200

    exo7 installation des middleware (Erreur 500)

--- exo7_middleware/server.js
+++ exo7_middleware/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  let start = performance.now();
   try {
     // ICI MIDDLEWARE A EXECUTER AVANT DE TRAITER LA REQUETE.
     console.log(req.httpVersion, req.url, req.method);
@@ -18,9 +19,11 @@ const server = http.createServer((req, res) => {
   } catch (err) {
     res.writeHead(500, {'content-type':'text/html'});
     res.write('<h1>500 Internal Server Error</h1>');
-    res.end()
   }
-} );
+  res.end();
+  let end = performance.now();
+  console.log("requete a pris", end - start);
+});
 
 server.listen(3000);
 console.log('Server écoute sur le port 3000');
